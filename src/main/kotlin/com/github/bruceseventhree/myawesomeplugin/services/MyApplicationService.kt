c70a098fa07d10ed17b149f5f9028e0722da8f66
GitHub Action <action@github.com>
2022-10-10 10:20:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/bruceseventhree/myawesomeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bruceseventhree/myawesomeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bruceseventhree.myawesomeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bruceseventhree.myawesomeplugin.MyBundle
 
 class MyApplicationService {
 
